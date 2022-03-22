9887fcc08746c49756536e4a8b5789eb4195ddcc
dymafr <erwan1309@hotmail.com>
2022-03-22 21:41:57 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,13 @@
 <script setup lang="ts">
 import TheHeader from './components/Header.vue';
 import TheFooter from './components/Footer.vue';
-
-import data from './data/product';
-import { computed, reactive } from 'vue';
-import type {
-  FiltersInterface,
-  ProductCartInterface,
-  ProductInterface,
-  FilterUpdate,
-} from './interfaces';
-import { DEFAULT_FILTERS } from './data/filters';
-
-const state = reactive<{
-  products: ProductInterface[];
-  cart: ProductCartInterface[];
-  filters: FiltersInterface;
-}>({
-  products: data,
-  cart: [],
-  filters: { ...DEFAULT_FILTERS },
-});
-
-function addProductToCart(productId: number): void {
-  const product = state.products.find((product) => product.id === productId);
-  if (product) {
-    const productInCart = state.cart.find(
-      (product) => product.id === productId
-    );
-    if (productInCart) {
-      productInCart.quantity++;
-    } else {
-      state.cart.push({ ...product, quantity: 1 });
-    }
-  }
-}
-
-function removeProductFromCart(productId: number): void {
-  const productFromCart = state.cart.find(
-    (product) => product.id === productId
-  );
-  if (productFromCart?.quantity === 1) {
-    state.cart = state.cart.filter((product) => product.id !== productId);
-  } else {
-    productFromCart.quantity--;
-  }
-}
-
-function updateFilter(filterUpdate: FilterUpdate) {
-  if (filterUpdate.search !== undefined) {
-    state.filters.search = filterUpdate.search;
-  } else if (filterUpdate.priceRange) {
-    state.filters.priceRange = filterUpdate.priceRange;
-  } else if (filterUpdate.category) {
-    state.filters.category = filterUpdate.category;
-  } else {
-    state.filters = { ...DEFAULT_FILTERS };
-  }
-}
-
-const cartEmpty = computed(() => state.cart.length === 0);
-
-const filteredProducts = computed(() => {
-  return state.products.filter((product) => {
-    if (
-      product.title
-        .toLocaleLowerCase()
-        .startsWith(state.filters.search.toLocaleLowerCase()) &&
-      product.price >= state.filters.priceRange[0] &&
-      product.price <= state.filters.priceRange[1] &&
-      (product.category === state.filters.category ||
-        state.filters.category === 'all')
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-});
+import Boutique from './features/boutique/Boutique.vue';
 </script>
 
 <template>
-  <div
-    class="app-container"
-    :class="{
-      gridEmpty: cartEmpty,
-    }"
-  >
+  <div class="app-container">
     <TheHeader class="header" />
+    <Component is:"Boutique" />
     <TheFooter class="footer" />
   </div>
 </template>
@@ -104,23 +24,8 @@ const filteredProducts = computed(() => {
   grid-template-rows: 48px auto 48px;
 }
 
-.gridEmpty {
-  grid-template-areas: 'header' 'shop' 'footer';
-  grid-template-columns: 100%;
-}
-
 .header {
   grid-area: header;
-}
-
-.shop {
-  grid-area: shop;
-}
-
-.cart {
-  grid-area: cart;
-  border-left: var(--border);
-  background-color: white;
 }
 
 .footer {
